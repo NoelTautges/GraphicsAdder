4fcbc53f19c5ca1ed723e172b983daa2eb38f847
Noel Tautges <24325426+NoelTautges@users.noreply.github.com>
2021-01-28 23:31:51 -0600

    Remove unused progress fields

--- GraphicsAdder.GUI/Models/ConversionProgress.cs
+++ GraphicsAdder.GUI/Models/ConversionProgress.cs
@@ -22,53 +22,5 @@ namespace GraphicsAdder.GUI.Models
             get => currentFile;
             set => this.RaiseAndSetIfChanged(ref currentFile, value);
         }
-        private int shaders = 1;
-        public int Shaders
-        {
-            get => shaders;
-            set => this.RaiseAndSetIfChanged(ref shaders, value);
-        }
-        private int currentShader = 0;
-        public int CurrentShader
-        {
-            get => shaders;
-            set => this.RaiseAndSetIfChanged(ref currentShader, value);
-        }
-        private int subShaders = 1;
-        public int SubShaders
-        {
-            get => shaders;
-            set => this.RaiseAndSetIfChanged(ref subShaders, value);
-        }
-        private int currentSubShader = 0;
-        public int CurrentSubShader
-        {
-            get => shaders;
-            set => this.RaiseAndSetIfChanged(ref currentSubShader, value);
-        }
-        private int passes = 1;
-        public int Passes
-        {
-            get => shaders;
-            set => this.RaiseAndSetIfChanged(ref passes, value);
-        }
-        private int currentPass = 0;
-        public int CurrentPass
-        {
-            get => shaders;
-            set => this.RaiseAndSetIfChanged(ref currentPass, value);
-        }
-        private int programs = 1;
-        public int Programs
-        {
-            get => shaders;
-            set => this.RaiseAndSetIfChanged(ref programs, value);
-        }
-        private int currentProgram = 0;
-        public int CurrentProgram
-        {
-            get => shaders;
-            set => this.RaiseAndSetIfChanged(ref currentProgram, value);
-        }
     }
 }
